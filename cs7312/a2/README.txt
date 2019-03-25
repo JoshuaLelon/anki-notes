a113251e930ed2c3000885c02de700077ea27584
Joshua Mitchell <jlm346@txstate.edu>
2019-03-24 20:26:39 -0500

    a3 draft done

---
+++ cs7312/a2/README.txt
@@ -0,0 +1,13 @@
+This project can be run using:
+
+- Python3.6
+- Pandas
+- Numpy
+
+The way to run this:
+
+python3 run.py <k> <input file name>
+
+example:
+
+python3 run.py 3 input1.txt
--- cs7312/a2/run.py
+++ cs7312/a2/run.py
@@ -10,16 +10,21 @@ def get_new_labels_iteration(X, centroids):
     return new_labels
 
 def kmeans(k, file_name):
-    data = pd.read_csv(file_name, header = None)
-
+    data = pd.read_csv(file_name, header = None, sep='\t')
+    print("k: ", k, "   filename: ", file_name)
     x1 = data[0].values
     x2 = data[1].values
     
-    centroid_x = np.random.randint(0, np.max(x1), size=k)
-    centroid_y = np.random.randint(0, np.max(x2), size=k)
+    # centroid_x = np.random.randint(np.mean(x1) - np.std(x1), np.mean(x1) + np.std(x1), size=k)
+    # centroid_y = np.random.randint(np.mean(x2) - np.std(x2), np.mean(x2) + np.std(x2), size=k)
+    centroid_x = np.array([np.max(x1) / (i + 1) for i in range(k)])
+    centroid_y = np.array([np.max(x2) / (i + 1) for i in range(k)])
+    # centroid_x = np.random.randint(0, np.max(x1), size=k)
+    # centroid_y = np.random.randint(0, np.max(x2), size=k)
     
     X = np.array(list(zip(x1, x2)))
     centroids = np.array(list(zip(centroid_x, centroid_y)))
+    
     temp_centroids = np.zeros(centroids.shape)
     cluster_labels = np.zeros(len(X))
 
@@ -31,8 +36,11 @@ def kmeans(k, file_name):
         for i in range(k):
             points = [X[j] for j in range(len(X)) if cluster_labels[j] == i]
             centroids[i] = np.mean(points, axis=0)
+            if np.isnan(centroids[i]).any(): # in case we end up having a centroid with no points in it
+                centroids[i][0] = np.random.randint(0, np.max(x1))
+                centroids[i][1] = np.random.randint(0, np.max(x2))
         error = distance(centroids, temp_centroids, None)
-        
+    colors = ['r', 'g', 'b', 'y', 'c', 'm']
     Out = np.array(list(zip(X[:, 0], X[:, 1], cluster_labels)))
     df = pd.DataFrame(Out)
     return df
