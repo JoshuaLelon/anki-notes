39811d23a7b8afa0516923db1df378ae578d1469
Joshua Mitchell <jlm346@txstate.edu>
2019-10-30 10:47:31 -0500

    need to write dataloder and classifier class

--- cs7313/proj/main.py
+++ cs7313/proj/main.py
@@ -1,12 +1,17 @@
 import torch
+import torch.nn as nn
+from model import SentimentClassifier
 
 def train(model, dataset, gpu, freeze_base, max_len, batch_size, learning_rate, print_per_n_lines, max_epochs):
 
     
 def evaluate(model, dataset, gpu):
     print("Evaluating " + model + " on the " + dataset + " dataset.")
-    dataloader?
-    criterion?
-    net?
-    
-    
+    dataloader = get_dataloader_for_dataset(dataset)
+    criterion = nn.BCEWithLogitsLoss()
+    net = SentimentClassifier()
+    net.cuda(gpu)
+    net.eval()
+
+def get_dataloader_for_dataset(dataset):
+    return dataset
