55e8192a85d77611c9ca3f6249954b8366574ccf
Joshua Mitchell <jlm346@txstate.edu>
2019-10-30 10:42:56 -0500

    outlining main.py

--- cs7313/proj/main.py
+++ cs7313/proj/main.py
@@ -1,5 +1,12 @@
+import torch
 
 def train(model, dataset, gpu, freeze_base, max_len, batch_size, learning_rate, print_per_n_lines, max_epochs):
 
     
 def evaluate(model, dataset, gpu):
+    print("Evaluating " + model + " on the " + dataset + " dataset.")
+    dataloader?
+    criterion?
+    net?
+    
+    
