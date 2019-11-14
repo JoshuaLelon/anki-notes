109f8ca80ce2ac48c564badb6d8167389aa76942
Joshua Mitchell <jlm346@txstate.edu>
2019-11-13 18:46:19 -0600

    creating naive bayes classifier

--- cs7313/proj/main.py
+++ cs7313/proj/main.py
@@ -7,23 +7,23 @@ from torch.utils.data import DataLoader
 from models.BERTSentiment import BERTSentimentClassifier
 from datasets.dataset_creator import get_val_dataloader_for_dataset, get_train_dataloader_for_dataset
 
-def train(model, dataset, freeze_base, max_len, batch_size, learning_rate, print_per_n_lines, max_epochs):
+def train_bert(dataset, freeze_base, max_len, batch_size, learning_rate, print_per_n_lines, max_epochs):
     gpu = 0
     if torch.cuda.is_available():
         gpu = 1
     return "implement me"
     
-def evaluate(model, dataset):
+def evaluate_bert(dataset):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     
     if torch.cuda.is_available():
         net.cuda(gpu)
         
     start_time = time.time()
-    print("Evaluating " + model + " on the " + dataset + " dataset using the " + str(device) + ".")
+    print("Evaluating BERT on the " + dataset + " dataset using the " + str(device) + ".")
     dataloader = get_val_dataloader_for_dataset(dataset)
     criterion = nn.BCEWithLogitsLoss()
-    net = BERTSentimentClassifier() # for now, we will automatically do BERT, later will generalize to any model
+    net = BERTSentimentClassifier() # for now, we will automatically do BERT, later will generalize to any 
     net.eval()
     
     mean_accuracy, mean_loss = 0, 0
@@ -49,3 +49,11 @@ def get_accuracy_from_logits(logits, labels):
     soft_probs = (probs > 0.5).long()
     acc = (soft_probs.squeeze() == labels).float().mean()
     return acc
+
+def train_nb(dataset):
+    gpu = 0
+    if torch.cuda.is_available():
+        gpu = 1
+    return "implement me"
+    
+def evaluate_nb(dataset):
